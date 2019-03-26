1ff0ca5a2142c55d1fab3666ada3ac483a81f2d2
pillisravani <pillisravani@100.111.213.204>
2019-03-26 11:46:27 -0700

    File read, write

--- LearningFiles/src/FileImport.java
+++ LearningFiles/src/FileImport.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class FileImport {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main(String[] args) throws IOException {
+			//open the file
+			FileInputStream fis = new FileInputStream("data.txt");
+			DataInputStream dis = new DataInputStream(fis);
+			BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+			String str;
+			//read the file
+			while((str=br.readLine())!=null) {
+				//print the file
+				System.out.println(str);
+			}
+			//close the file
+			dis.close();
+		
 	}
 
 }
